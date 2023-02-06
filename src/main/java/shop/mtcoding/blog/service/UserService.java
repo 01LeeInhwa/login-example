962669851b01d3697a4ee7f73b9abd1cece3256e
Inhwa <2inhwa5@gmail.com>
2023-02-06 23:03:39 +0900

    회원가입 컨트롤러 완료

---
+++ src/main/java/shop/mtcoding/blog/service/UserService.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blog.service;
+
+import org.springframework.stereotype.Service;
+
+import shop.mtcoding.blog.dto.user.UserReq.JoinReqDto;
+
+@Service
+public class UserService {
+    public int 회원가입(JoinReqDto joinReqDto) {
+        return -1;
+    }
+
+}
